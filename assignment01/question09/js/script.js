829cd378971b30253d1caa059e2e7899f574ee96
leahtard <leah@leahmarsh.com>
2013-09-14 15:59:52 -0700

    Complete assignemnt #1, question #9.

---
+++ assignment01/question09/js/script.js
@@ -0,0 +1,10 @@
+function createUser() {
+  var firstName = prompt('Enter your FIRST name:','');
+  var lastName = prompt('Enter your LAST name:','');
+  var userName = firstName+'.'+lastName;
+  alert('Thank you! Your username is '+userName);
+}
+
+function enterUser() {
+  prompt('Please enter your username:', '');
+}
