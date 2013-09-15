d6594abb78d23c4ad48e7525f335f856a397d22a
leahtard <leah@leahmarsh.com>
2013-09-14 21:15:52 -0700

    Commit assignemnt #1, question #17. Still work in progress.

---
+++ assignment01/question17/js/script.js
@@ -0,0 +1,9 @@
+var x = prompt('Enter number x:', '');
+var y = prompt('Enter number y:', '');
+var z = (parseInt(x)+1) + parseInt(y);
+
+alert(z); 
+
+if (z > 10) {
+  alert('Count is greater than ten.');
+}
