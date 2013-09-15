ab3df0a61846bec6ac32d9ffae7a517e0509d5c3
leahtard <leah@leahmarsh.com>
2013-09-15 11:20:06 -0700

    Complete assignemnt #1, question #10.

---
+++ assignment01/question10/js/script.js
@@ -0,0 +1,6 @@
+var c, thisIsAVariable, q76354, number;
+
+var c = alert('Variable, c, has been defined.');
+var thisIsAVariable = alert('Variable, thisIsAVariable, has been defined.');
+var q76354 = alert('Variable, q76354, has been defined.');
+var number = alert('Variable, number, has been defined.');
