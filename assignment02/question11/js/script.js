0298cb344fd8b5949db466d17e3671bd1409a5d0
leahtard <leah@leahmarsh.com>
2013-09-21 17:22:38 -0700

    Complete assignment #2, question #11.

---
+++ assignment02/question11/js/script.js
@@ -0,0 +1,13 @@
+function foreverLoop() {
+	
+	var x = 0;
+
+	while (x = 0) {
+		var counter = 1
+		document.write(counter * 2+'&nbsp;');
+		counter++;
+	}
+
+	// Script will not run if there is an infinte loop
+
+}
