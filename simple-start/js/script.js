19c527706a6c5e69d4665826256b4b5fe1636ae2
leahtard <leah@leahmarsh.com>
2013-09-28 11:31:59 -0700

    Update to simple start file structure.

--- simple-start/js/script.js
+++ simple-start/js/script.js
@@ -1,3 +1,15 @@
-function func() {
+function process() {
 	alert('Hello World');
+
+	// Function for writing results
+	function outputResults(id, variable, message) {
+		document.getElementById(id).innerHTML = '<h2>'+message+' '+variable+'</h2>';
+	}
+
+	// Calulate result for output
+	var result = "Result output here...";
+
+	// Print results to page with function
+	outputResults("results", result, "The results are:")
+
 }
