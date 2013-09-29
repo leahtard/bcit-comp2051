59715d0c321a2c5088ee6bb323b16f128f6d2935
leahtard <leah@leahmarsh.com>
2013-09-28 21:24:06 -0700

    Push assignment #3, question #2. Still work in progress.

--- assignment03/question02/js/script.js
+++ assignment03/question02/js/script.js
@@ -1,27 +1,33 @@
-//function calculateCharges(h) {
-
-
-
-//}
-
-//calculateCharges('sdasd');
-
-// Create Data Array
-//var data = new Array(1,2,3);
-
-
-
+// Ask how many cars parked
+var num = parseInt(prompt('How many cars parked in the lot today?', ''));
 
 // Collect data in array
+
+
+// Output the correct number for form fields
 function formData() {
-	var hours = document.getElementById("hours").value;	
-	var data = new Array();
-	data.push(1);
-	data.push(hours);
-	return data;
+	for (var i=0;i<num;i++) {
+		document.write('Hours for car '+(i+1)+': <input type="text" name="hours" id="hours"> <br />');
+		//data.push(1);
+		//data.push(hours);
+		//return data;
+	};
+	
 }
+formData();
 
-
+// Collect
+function submitHours() {
+	var data = new Array();
+	var hours = document.getElementById("hours").value;
+	for (var i=0; i<hours.length; i++) {
+	data.push(hours);
+	}
+	for (var i=0; i<data.length; i++) {
+	  alert(data[i]);
+	  // Do something with element i.
+	}	
+}
 
 //document.write(results);
 
