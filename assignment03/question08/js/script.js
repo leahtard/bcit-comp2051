780a853b1676b2feca6ff06e4919b82b0c4d5606
leahtard <leah@leahmarsh.com>
2013-09-28 21:32:28 -0700

    Push assignment #3, question #8. Still work in progress.

---
+++ assignment03/question08/js/script.js
@@ -0,0 +1,13 @@
+function process() {
+	
+	function quotientInt(a, b) {
+		var r = parseInt(a / b);
+		return r;
+	}
+
+	function quotientRem(a, b) {
+		var r = parseInt(a % b);
+		return r;
+	}
+
+}
