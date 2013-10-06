ac1518a7f7fd89757df1214a8745d050272d4933
leahmarsh <leah@thejibe.com>
2013-10-05 22:03:40 -0700

    Push assignment #4, question 01g01j. Still work in progress.

---
+++ assignment04/question01g01j/js/script.js
@@ -0,0 +1,7 @@
+function process() {
+
+	var items = [['1a','2a','3a','4a'],['1b','2b','3b','4b','5b'],['1c','2c','3c']];
+	var itemsRow = items.length;
+	document.getElementById('results').innerHTML = '<h2>This array has '+itemsRow+' rows</h2>';
+
+}
