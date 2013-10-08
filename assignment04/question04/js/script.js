5d8777ce28e0d495bfc8ad66c34ff09bb1c49ab4
leahtard <leah@leahmarsh.com>
2013-10-07 20:27:19 -0700

    Push assignment #4, question #4. Still work in progress.

--- assignment04/question04/js/script.js
+++ assignment04/question04/js/script.js
@@ -1,7 +1,14 @@
-function randomNum() {
-	return Math.floor(Math.random() * 90) + 10;
+// Function for writing results
+function outputResults(id, variable, message) {
+	document.getElementById(id).innerHTML = '<h2>'+message+' '+variable+'</h2>';
 }
 
+// Random number generator
+function randomNum() {
+	return Math.floor(Math.random() * 20) + 10;
+}
+
+// Add random number to array
 function randomArray(n) {
 	var data = new Array;
 	for (var i=0; i<n; i++) {
@@ -11,25 +18,22 @@ function randomArray(n) {
 	return data;
 }
 
+// Create random array
 var array = randomArray(20);
-var uniqueNums = new Array;
-
-
-
-alert(array);
 
 function process() {
-	alert('Hello World');
 
-	// Function for writing results
-	function outputResults(id, variable, message) {
-		document.getElementById(id).innerHTML = '<h2>'+message+' '+variable+'</h2>';
+	var uniqueNums = new Array;
+
+	for (var i=0; i<array.length; i++) {
+		alert(array[i]);
+		uniqueNums.push(array[i]); // Need to only push if it isn't already there???
 	}
 
 	// Calulate result for output
-	var result = "Result output here...";
+	var result = uniqueNums;
 
 	// Print results to page with function
-	outputResults("results", result, "The results are:")
+	outputResults("results", result, "The unique numbers in the array are:")
 
 }
